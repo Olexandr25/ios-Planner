7fe43bccb4fa70cca90b53413c242d9a32274712
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2021-12-03 14:33:55 +0200

    Found all icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { AiOutlineSearch } from "react-icons/ai"
+import { Icons } from "./components/Icons/"
+
 function App() {
   return (
     <div className="App">
       <h1>App.js</h1>
+      <Icons />
     </div>
   )
 }
---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,33 @@
+import { AiOutlineSearch, AiOutlineUnorderedList, AiOutlinePlus } from "react-icons/ai"
+import { 
+    BsCalendar3, 
+    BsFillInboxFill, 
+    BsFillFlagFill, 
+    BsCalendarDate, 
+    BsClock, 
+    BsPlusCircle, 
+    BsFillPlusCircleFill
+} from "react-icons/bs"
+import { FaLocationArrow } from "react-icons/fa"
+import { HiOutlineInformationCircle } from "react-icons/hi"
+
+const Icons = () => {
+  return (
+    <>
+      <div> Search - <AiOutlineSearch /> </div>
+      <div> Calendar - <BsCalendar3 /> </div>
+      <div> Today - <BsCalendarDate /> </div>
+      <div> All - <BsFillInboxFill /> </div>
+      <div> Flag - <BsFillFlagFill /> </div>
+      <div> List - <AiOutlineUnorderedList /> </div>
+      <div> Clock - <BsClock /></div>
+      <div> Location - <FaLocationArrow /> </div>
+      <div> Information - <HiOutlineInformationCircle /> </div>
+      <div> Plus first - <AiOutlinePlus /> </div>
+      <div> Plus second outline - <BsPlusCircle /> </div>
+      <div> Plus third fill - <BsFillPlusCircleFill /></div>
+    </>
+  )
+}
+
+export default Icons
