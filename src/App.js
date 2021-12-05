238d3eb0da89841c50304befad5fd42711444061
Oleksandr25 <oleksandrvoichyshyn@gmail.com>
2021-12-05 17:16:10 +0200

    Wrote jsconfig.json for comfortable importing files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './styles/styles.css';
-import { Switcher } from "./components/Switcher"
+import "./styles/styles.css"
+import { Switcher } from "components/Switcher"
 
 function App() {
   return (
