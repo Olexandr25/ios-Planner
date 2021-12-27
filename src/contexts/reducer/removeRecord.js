98cd211c0b68a0e360646590779afe491164fc2b
Oleksandr25 <oleksandrvoichyshyn@gmail.com>
2021-12-27 14:57:51 +0200

    Upgrated removeRecord, when remove category, check all his task, and remove them too

--- src/contexts/reducer/removeRecord.js
+++ src/contexts/reducer/removeRecord.js
@@ -1,13 +1,20 @@
 const removeRecord = (state, payload) => {
   const { collectionPath, id } = payload
 
-  // if (collectionPath === "category") {
   const removeIndex = state[collectionPath]?.findIndex(item => item.id === id)
   state[collectionPath]?.splice(removeIndex, 1)
-  // } else {
-  //   const removeIndex = state[collectionPath]?.findIndex(item => item.id === id)
-  //   state[collectionPath]?.splice(removeIndex, 1)
-  // }
+
+  if (collectionPath === "category") {
+    console.log(collectionPath)
+    state?.task?.map(item => {
+      let x = 0
+      if (item.categoryId === id) {
+        state?.task?.splice(x, 1)
+      }
+      x++
+      return item
+    })
+  }
 
   return { ...state }
 }
